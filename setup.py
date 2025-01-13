147e51d9d5bc9dfc01161afa51ba4e3a79c01731
Jake Mannix <jake.mannix@gmail.com>
2025-01-13 09:56:53 -0800

    add pytest to top level setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
             "black>=23.7.0",
             "ruff>=0.0.286",
             "mypy>=1.5.1",
+            "pytest>=7.4.0",
+            "pytest-asyncio>=0.21.1",
+            "pytest-cov>=4.1.0",
         ]
     },
     python_requires=">=3.10"
